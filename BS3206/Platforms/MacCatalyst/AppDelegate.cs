f054a72a16b8f42921a36a36165067fce98ee193
clarkec22 <c.clarke.22@unimail.winchester.ac.uk>
2025-05-02 12:13:38 +0100

    Initial commit

---
+++ BS3206/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BS3206
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
