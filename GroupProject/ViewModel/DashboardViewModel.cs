1afc6933051e2e5ab047bd26ec5ead46440c80ac
nate <nate>
2025-05-14 18:18:01 +0100

    main-2-wip

--- GroupProject/ViewModel/DashboardViewModel.cs
+++ GroupProject/ViewModel/DashboardViewModel.cs
@@ -5,15 +5,29 @@ namespace GroupProject.ViewModels
     public class DashboardViewModel
     {
         public ICommand GoToAccountCommand { get; }
+		public ICommand GoToLogoutCommand { get; }
+		public ICommand GoToSandboxCommand { get; }
 
         public DashboardViewModel()
         {
             GoToAccountCommand = new Command(async () => await GoToAccountPageAsync());
+			GoToLogoutCommand = new Command(async () => await GoToLogoutPageAsync());
+			GoToSandboxCommand = new Command(async () => await GoToSandboxPageAsync());
         }
 
         private async Task GoToAccountPageAsync()
         {
             await Shell.Current.GoToAsync("///AccountPage");
+        }
+
+		private async Task GoToLogoutPageAsync()
+        {
+            await Shell.Current.GoToAsync("///LoginPage");
+        }
+
+        private async Task GoToSandboxPageAsync()
+        {
+            await Shell.Current.GoToAsync("///PuzzlePage");
         }
     }
 }
