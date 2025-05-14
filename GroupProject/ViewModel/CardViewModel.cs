8ad38a447229d80411dfe632c4ec46083c806574
nate <nate>
2025-05-14 04:37:30 +0100

    still buggy, but major progress

--- GroupProject/ViewModel/CardViewModel.cs
+++ GroupProject/ViewModel/CardViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace GroupProject.ViewModel
 {
     public class CardViewModel : INotifyPropertyChanged
     {
-        public int Id { get; }
+        public int Id { get; set;}
         public string GateType { get; }
-        public string DisplayName => $"{GateType} #{Id}";
+        public string DisplayName => $"{GateType}"; // #{Id}"; // Temp remove id
 
         double _x;
         public double X { get => _x; set { _x = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(X))); } }
@@ -20,8 +21,37 @@ namespace GroupProject.ViewModel
             GateType = gateType;
             X = startX;
             Y = startY;
+
+			CurrentValue = gateType == "Output" ? "0" : string.Empty;
         }
 
+		// protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		// {
+		// 	PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		// }
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+
+		// Output card stuff
+
+		private string _currentValue;
+		public string CurrentValue
+		{
+			get => _currentValue;
+			set
+			{
+				if(_currentValue != value)
+				{
+					_currentValue = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
     }
 }
