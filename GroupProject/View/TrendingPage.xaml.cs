452db16dd4c76f3bc606211f817f2d8c379992e8
nate <nate>
2025-05-17 13:05:22 +0100

    Redesigned Dashboard Page with Embedded Trending

--- GroupProject/View/TrendingPage.xaml.cs
+++ GroupProject/View/TrendingPage.xaml.cs
@@ -2,16 +2,11 @@ using GroupProject.ViewModel;
 
 namespace GroupProject.View;
 
-public partial class TrendingPage : ContentPage
+public partial class TrendingPage : ContentView
 {
     public TrendingPage()
     {
         InitializeComponent();
         BindingContext = new TrendingPageViewModel();
-    }
-
-    private async void OnBackToDashboardClicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("///DashboardPage");
     }
 }
