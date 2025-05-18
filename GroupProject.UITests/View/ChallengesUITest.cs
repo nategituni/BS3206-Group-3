7a95eac54d73e395c8dfadc6cad85d37210246b7
MEddleston <michael.eddleston@icloud.com>
2025-05-18 13:46:52 +0100

    Challenges Page Automated Testing

---
+++ GroupProject.UITests/View/ChallengesUITest.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+
+namespace GroupProject.UITests.View;
+
+public class ChallengesPageUITest : BaseTest
+{
+    public void NavigateToChallengesPage()
+    {
+        var emailEntry = FindUIElement("LoginEmailEntryField");
+        var passwordEntry = FindUIElement("LoginPasswordEntryField");
+        var loginButton = FindUIELement("LoginBtn");
+
+        emailEntry.Click();
+        emailEntry.SendKeys("michael.eddleston@icloud.com");
+        Thread.Sleep(1000)
+
+        passwordEntry.Click();
+        passwordEntry.SendKeys("Password1!");
+        Thread.Sleep(1000);
+
+        loginButton.Click();
+        Thread.Sleep(1000);
+
+        var ChallengesPageButton = FindUIElement("ChallengesPageBtn");
+
+        Assert.That(ChallengesPageButton != null, "Challenges Page button not found.");
+
+        ChallengesPageButton.Click();
+        Thread.Sleep(1000);
+    }
+
+    [Test]
+    public void ChallengesPageInitialiation()
+    {
+        NavigateToChallengesPage()
+
+        var challengeText = findUIElement("ChallengeText");
+
+        Assert.That(challengeText != null, "Challenges not found.")
+    }
+
+}
